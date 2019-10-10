e532365480016ea2633121e90b8493c16695b4b0
afsk1240 <afsk1240@gmail.com>
2019-10-10 17:05:11 +0900

    Add Get Customers Rest API

--- server.js
+++ server.js
@@ -7,8 +7,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+            'id' :1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '홍길동',
+            'birthday' : '961222',
+            'gender' : '남자',
+            'job' : '대학생'
+        },
+        {
+            'id' :2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '테스트',
+            'birthday' : '960101',
+            'gender' : '남자',
+            'job' : '대학생'
+        },
+        {
+            'id' :3,
+            'image' : 'https://placeimg.com/64/64/3',
+            'name' : '나동빈',
+            'birthday' : '960305',
+            'gender' : '남자',
+            'job' : '대학생'
+        }
+    ]);
 });
 
 
