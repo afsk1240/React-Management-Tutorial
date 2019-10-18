c3d22b733985758984126e7aff353939d020f1b4
afsk1240 <afsk1240@gmail.com>
2019-10-18 14:18:57 +0900

    Add Get Customers Rest API

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+const data =fs.readFileSync('./database.json');
+
+
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+
+connection.connect();
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-            'id' :1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '홍길동',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-        },
-        {
-            'id' :2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '테스트',
-            'birthday' : '960101',
-            'gender' : '남자',
-            'job' : '대학생'
-        },
-        {
-            'id' :3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '나동빈',
-            'birthday' : '960305',
-            'gender' : '남자',
-            'job' : '대학생'
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err,rows,fields) => {
+            res.send(rows);
         }
-    ]);
+    )
 });
 
 
